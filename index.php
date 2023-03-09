c4abc9a50d3e32ec6882a938ea9e35a1859e1ab7
mrisho99 <mrishohamisi2348@gmail.com>
2023-03-09 21:48:22 +0300

    eigth

--- index.php
+++ index.php
@@ -122,6 +122,7 @@
                   <div><a href="#">VIDEO SHOOTING</a></div>
                   <div><a href="#">GRAPHICS DESIGN</a></div>
                   <div><a href="#">WEB DESIGN</a></div>
+                  <div><a href="background.php">our background</a></div>
                   <div><a href="#">ONLINE RADIO</a></div>
                 </div>
                 <div class="right box">
